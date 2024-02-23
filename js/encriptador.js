586a5a3079e472a9d3f8527f25066234408c6e8c
ratatouille99 <115515737+ratatouille99@users.noreply.github.com>
2024-02-22 19:35:03 -0500

    'Primer'

---
+++ js/encriptador.js
@@ -0,0 +1,61 @@
+let verificar = true;
+const valores = {
+    "e" :"enter",
+    "i" :"imes",
+    "a" :"ai",
+    "o" : "ober",
+    "u" :"ufat"
+};
+
+function encript(texto){
+    console.log(texto.length);
+    
+    if (/[A-Z0-9]/.test(texto)) {
+        alert("Solo puedes usar letras minúsculas sin mayúsculas ni números!!");
+    } else if(texto.length > 0){
+        if(verificar == true){    
+            document.getElementById("mostrar").style.display = "none";
+            document.getElementById("oculto").style.display = "block";
+            verificar = false;
+        }
+
+        for(let key in valores){
+            if(valores.hasOwnProperty(key)){
+                texto = texto.replaceAll(key, valores[key]);
+            }
+        }
+        document.getElementById("devolver-texto").textContent = texto;
+    } else{
+        document.getElementById("cambio-texto").textContent="No te quieras pasar de listo manito";
+    }
+}
+
+function decript(texto){
+    console.log(texto);
+
+    if (/[A-Z0-9]/.test(texto)) {
+        alert("Solo puedes usar letras minúsculas sin mayúsculas ni números!!");
+    } else if(texto.length > 0){
+        if(verificar == true){    
+            document.getElementById("animacion").style.animation;
+            document.getElementById("mostrar").style.display = "none";
+            document.getElementById("oculto").style.display = "block";
+            verificar = false;
+        }
+    
+        for(let key in valores){
+            if(valores.hasOwnProperty(key)){
+                texto = texto.replaceAll(valores[key],key);
+            }
+        }
+        document.getElementById("devolver-texto").textContent = texto;
+    } else{
+        document.getElementById("cambio-texto").textContent="No te quieras pasar de listo manito";
+    }
+}
+
+function copy() {
+    let copyText = document.querySelector("#devolver-texto");
+    copyText.select();
+    document.execCommand("copy");
+}
